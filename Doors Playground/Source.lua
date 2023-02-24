6c8b6bae8234b639d2e97113a49d29b311c18441
Christolius <92441268+Christolius@users.noreply.github.com>
2023-02-24 14:46:23 +0700

    im so stupid

--- Doors Playground/Source.lua
+++ Doors Playground/Source.lua
@@ -16,19 +16,19 @@ local SPWNS = Window:AddTab("Summon", {default = false})
 -- Create Section
 local LE = SPWNS:AddSection("Local Entities", {default = false})
 
-v49 = game:GetService("Players").totosimamora608.PlayerGui.MainUI.Initiator
+v49 = game:GetService("Players").LocalPlayer.PlayerGui.MainUI.Initiator
 v59 = v49.Main_Game.SpiderJumpscare
 
 i = 100
 sel = "Spider"
-local rp = LE:AddBox("Repeat Amount", {fireonempty = true}, function(text)
+local rp = LE:AddBox("Repeat", {fireonempty = true}, function(text)
 	if type(tonumber(text)) == "number" then
 		i = tonumber(text)
 	elseif text == nil or text == "" then
 		i = 1	
 	end
 end)
-local Dropdown = LE:AddDropdown("Select", {"A-90","Glitch","Halt","Screech","Timothy","Void"}, {default = "Item1"}, function(selected)
+local Dropdown = LE:AddDropdown("Select", {"A-90","Glitch","Halt","Seek","Screech","Timothy","Void"}, {default = "Item1"}, function(selected)
 	sel = selected
 end)
 local strt = LE:AddButton("Summon", function()
@@ -38,13 +38,15 @@ local strt = LE:AddButton("Summon", function()
 	elseif sel == "Void" then
 		func = require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Void).stuff
 	elseif sel == "Screech" then
-		func = require(game:GetService("Players").totosimamora608.PlayerGui.MainUI.Initiator.Main_Game.Screech)
+		func = require(game:GetService("Players").LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Screech)
 	elseif sel == "Glitch" then
 		func = require(v49.Main_Game.Glitch).stuff
 	elseif sel == "A-90" then
 		func = require(v49.Main_Game.A90)
 	elseif sel == "Halt" then
 		func = require(v49.Main_Game.Shade).stuff		
+	elseif sel == "Seek" then
+		func = require(v49.Main_Game.Seek)		
 	end
 	idx = i
 	repeat
