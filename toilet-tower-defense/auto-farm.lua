b28bd4890f0c70240aa0328a0971af3e1c1d1c6b
Christolius <92441268+Christolius@users.noreply.github.com>
2023-09-10 16:35:44 +0700

    Update auto-farm.lua

--- toilet-tower-defense/auto-farm.lua
+++ toilet-tower-defense/auto-farm.lua
@@ -4,10 +4,8 @@ while not game:IsLoaded() do task.wait() end
 if not game.ReplicatedStorage:FindFirstChild'IsLobby' then return end
 local map = "ToiletHQ"
 if game.ReplicatedStorage.IsLobby.Value then
-    task.spawn(function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Christolius/Roblox/main/toilet-tower-defense/auto-start.lua'),true)()
-    end)
-    wait(.5)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Christolius/Roblox/main/toilet-tower-defense/auto-start.lua'),true)()
+    wait()
     local lift = workspace.Lifts:GetChildren()[math.random(1,#workspace.Lifts:GetChildren())]
     while lift.Name ~= map do
         task.wait()
@@ -15,7 +13,5 @@ if game.ReplicatedStorage.IsLobby.Value then
     end
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = lift.Area.CFrame
 else
-    task.spawn(function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Christolius/Roblox/main/toilet-tower-defense/enable-auto-skip.lua'),true)()
-    end)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Christolius/Roblox/main/toilet-tower-defense/enable-auto-skip.lua'),true)()
 end
