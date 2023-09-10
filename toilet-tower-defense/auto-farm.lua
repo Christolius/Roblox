2e5b86cd3be50fe674019af7583c10f535e86f17
Christolius <92441268+Christolius@users.noreply.github.com>
2023-09-10 16:49:44 +0700

    Update auto-farm.lua

--- toilet-tower-defense/auto-farm.lua
+++ toilet-tower-defense/auto-farm.lua
@@ -5,6 +5,7 @@ wait(5)
 if not game.ReplicatedStorage:FindFirstChild'IsLobby' then return end
 local map = "ToiletHQ"
 if game.ReplicatedStorage.IsLobby.Value then
+    print('Lobby Script Loaded')
     spawn(function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/Christolius/Roblox/main/toilet-tower-defense/auto-start.lua'),true)()
     end)
