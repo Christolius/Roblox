25d12f421d6bea5cf4464b509a0c32332370f3d9
Christolius <92441268+Christolius@users.noreply.github.com>
2023-09-10 16:48:51 +0700

    Update auto-farm.lua

--- toilet-tower-defense/auto-farm.lua
+++ toilet-tower-defense/auto-farm.lua
@@ -1,12 +1,14 @@
 -- PUT THIS IS AUTOEXEC FOLDER
 game.Players.LocalPlayer.CharacterAdded:Wait()
 while not game:IsLoaded() do task.wait() end
-wait(10)
+wait(5)
 if not game.ReplicatedStorage:FindFirstChild'IsLobby' then return end
 local map = "ToiletHQ"
 if game.ReplicatedStorage.IsLobby.Value then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Christolius/Roblox/main/toilet-tower-defense/auto-start.lua'),true)()
-    wait()
+    spawn(function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Christolius/Roblox/main/toilet-tower-defense/auto-start.lua'),true)()
+    end)
+    wait(.1)
     local lift = workspace.Lifts:GetChildren()[math.random(1,#workspace.Lifts:GetChildren())]
     while lift.Name ~= map do
         task.wait()
